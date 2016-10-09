11da6512af6d17df62681660e6b05eb139011f93
Hegeljoseph <hegeljoseph@gmail.com>
2016-10-09 21:21:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,30 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: "Article one | Hegel Joseph",
+    heading: "Article One",
+    date: 'Sep 5, 2016',
+    content: `
+      <p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+                </p>
+                <p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+                </p>
+                <p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+</p>`
+};
+
+
+var htmlTemplate =`
+
+
+
+
+`;
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
