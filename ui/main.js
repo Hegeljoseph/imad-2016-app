d49cc5e10be228ea95413d19c3a2457d0135d7fb
Hegeljoseph <hegeljoseph@gmail.com>
2016-10-10 16:48:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,23 @@ var button = document.getElementById('counter');
 var counter=0;
 
 button.onclick = function () {
+    
+    // Make a request
+    var request = new XMLHttpRequest();
 
-counter= counter + 1;
-var span = document.getElementById('count');
-span.innerHTML = counter.toString();
+    //capture the response and store in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    request.open('GET','http://hegeljoseph.imad.hasura-app.io/counter',true);
+    request.send(null);
+
+
 };
