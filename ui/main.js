e2e141815f1840ffeea1a43d4f0fe9bcbc07d5ec
Hegeljoseph <hegeljoseph@gmail.com>
2016-10-10 16:37:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,10 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementById('counter');
+var counter=0;
 
-var element = document.getElementById('main-text');
+button.onclick = function () {
 
-element.innerHTML = 'New value';
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-img.onclick = function () {
-    var interval = setInterval(moveRight,10);
-    img.style.marginLeft = '100px';
+counter= counter + 1;
+var span = document.getElementById('count');
+span.innerHTML = counter.toString();
 };
