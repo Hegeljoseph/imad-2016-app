dba65ab538ad004ee019d095be1c57bace6daeb3
Hegeljoseph <hegeljoseph@gmail.com>
2016-10-11 10:33:28 +0530

    Create articles.js

---
+++ ui/articles.js
@@ -0,0 +1,32 @@
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    
+      
+    // Make a request
+    var request = new XMLHttpRequest();
+
+    //capture the response and store in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200){
+                var comments = request.responseText;
+                comments = JSON.parse(comments);
+                var list = '';
+                for(var i=0; i < comments.length;i++){
+                    list += '<li>' + comments[i] + '</li>';
+                }
+                var ul=document.getElementById('commentlist');
+                ul.innerHTML = list;
+            }
+        }
+    };
+    
+    var commentInput = document.getElementById('comment');
+    var comment = commentInput.value;
+    request.open('GET','http://hegeljoseph.imad.hasura-app.io/submit-comment?comment=' + comment,true);
+    request.send(null);
+
+    
+    
+    
+}
