a69caf6ad8303136cf32dbe49bb6aab7ba049d23
Hegeljoseph <hegeljoseph@gmail.com>
2016-10-11 09:59:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,8 +24,7 @@ button.onclick = function () {
 
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     
@@ -49,6 +48,8 @@ submit.onclick = function () {
         }
     };
     
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://hegeljoseph.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
 
