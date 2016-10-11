454291eafe3c1092ae698e353efd8d29bf218540
Hegeljoseph <hegeljoseph@gmail.com>
2016-10-11 10:36:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,6 +139,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/articles.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'articles.js'));
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
